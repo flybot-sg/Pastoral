fce877e7907e18d00e487da854496940ae541f35
Isaac Johnston <isaac@basecity.com>
2018-04-20 16:11:05 +0800

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject pastoral "0.0.1-SNAPSHOT"
+  :description "TBA"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+
+  :source-paths ["src"])
+
