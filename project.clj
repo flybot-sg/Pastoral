97ca66872747ee0d458ee7f62018519267e6f85c
Isaac Johnston <isaac@basecity.com>
2018-08-16 11:07:35 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject pastoral "0.0.1-SNAPSHOT"
-  :description "TBA"
+  :description "Derived Values, Flowing, for Arcadia (Clojure in Unity)."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
